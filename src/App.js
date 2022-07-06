ffa6216670a9fe49a401b5bf671f5d5ac9f6deca
Pradyumn Garg <86672147+Pradyumn-Garg@users.noreply.github.com>
2022-07-06 21:36:34 +0530

    commit 2
    This commit includes following features:
    1) On home page, switching flows and account tabs no more clears up created flows. Bug fixed using UseContext hook.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
 import './index.css';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'  
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Login from './components/login'
 import Home from './components/home'
 import Reactflow from './components/reactflow';
 import Flows from './components/flows';
 
-const routing = (data,setData) => (
+const routing = (
   <Router>
     <Routes>
-      <Route exact path="/" element={<Login data={data} setData={setData}/>} />  
-      <Route exact path="/home" element={<Home data={data} setData={setData}/>} />  
-      <Route exact path="/reactapp" element={<Reactflow data={data} setData={setData}/>} />
-      <Route exact path="/flowlist" element={<Flows data={data} setData={setData}/>} />
-      
-    </Routes>  
-  </Router>  
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/reactapp" element={<Reactflow />} />
+      <Route exact path="/flowlist" element={<Flows />} />
+    </Routes>
+  </Router>
 )
 
 export default function App() {
-  const [data, setData] = useState([]);
-  return (routing(data,setData));
+  return (routing);
 }
