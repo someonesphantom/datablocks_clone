7eeb91d8768b6aa646cb0cca88f16300d23dfca9
Tummala Venkat <venkatin2sl@gmail.com>
2022-07-19 17:51:53 +0800

    added nodes

--- src/components/e-insta/main/reactflow.js
+++ src/components/e-insta/main/reactflow.js
@@ -49,81 +49,9 @@ import { FlowContext } from '../context/flowcontext';
 import apiMapping from '../../resources/apiMapping.json';
 import axios from 'axios';
 import './reactflow.scss'
+import Filenode from '../nodes/filenode';
 
-const SourceNode = ({ data }) => {
-  if (data.color === "") {
-    data.color = "333154"
-  }
-  const [value, displayresponse, parsedData, tablerows, Values] = useResponse(null)
-
-
-  const { tableRows, setTableRows, values, setValues, filetype, setFileType } = useContext(UserContext)
-  useEffect(() => {
-    console.log("Table Rows ", tablerows)
-    setTableRows(tablerows)
-  }, [tablerows])
-  useEffect(() => {
-    console.log("values  ", Values)
-
-    setValues(Values)
-  }, [Values])
-  useEffect(() => {
-    console.log("fileType", filetype)
-
-  }, [filetype])
-
-  return (
-    <>
-      <UserContextProvider >
-        <Box className="boxf1" sx={{  borderColor: "#" + data.color}}>
-          <Card variant="outlined" className='cardf1' >
-            <CardHeader className='cd' style={{ backgroundColor: "#" + data.color, border: 1, borderColor: "#" + data.color, borderRadius: 2 }} />
-            <React.Fragment>
-              <CardContent>
-                <left>
-                  <DragIndicatorIcon sx={{ fontSize: "30px", position: "absolute", left: "5px", top: "5px", color: "white" }} />
-                  <Typography fontSize="15px" position="absolute" left="30px" top="8px" color="white">
-                    File
-                  </Typography>
-
-                </left>
-                <input
-                  type="file"
-                  name="file"
-                  onChange={(event) => {
-                    displayresponse(event);
-                    { console.log("event", event) }
-                    setFileType(event.target.files[0].type)
-                  }}
-                  accept=".csv, .json"
-                  className='inpf1'
-                />
-                <Typography  className='tyf1'  color="white" gutterBottom>
-                  {data.value}
-                </Typography>
-
-                <Typography variant="body2" color="text.secondary">
-
-                </Typography>
-
-              </CardContent>
-
-            </React.Fragment>
-          </Card>
-        </Box>
-        <Handle
-          type="source"
-          position="right"
-          id="a"
-          className='handleright'
-          isConnectable={true}
-        />
-      </UserContextProvider>
-    </>
-  )
-}
-
-const nodeTypes = { source: SourceNode };
+const nodeTypes = { source: Filenode };
 const edgeTypes = {
   // custom: CustomEdge,
 };
